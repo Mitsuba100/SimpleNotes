cd659cf73502cfdcd06c31d3238c596b0fd2cda6
Mitsuba100 <stui.drew@outlook.de>
2026-01-02 23:21:11 +0100

    fixing ui making more consistan making a button do button stuff lul

--- app/src/main/java/net/stuple/simplenotes/FileManagment.java
+++ app/src/main/java/net/stuple/simplenotes/FileManagment.java
@@ -2,3 +2,10 @@ package net.stuple.simplenotes;
 
 public class FileManagment {
 }
+/*
+1.
+Datein lessen
+dann datein ins array
+dann kann man in menu auswälen
+2. Datein in
+ */
--- app/src/main/java/net/stuple/simplenotes/SettingsFragment.java
+++ app/src/main/java/net/stuple/simplenotes/SettingsFragment.java
@@ -1,9 +1,18 @@
 package net.stuple.simplenotes;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+
+
+import androidx.annotation.Nullable;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -16,8 +25,18 @@ public class SettingsFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = SettingsFragmentBinding.inflate(inflater, container, false);
-        double version = 1.1;
-        binding.textView3.setText(new StringBuilder().append("Version: ").append(version).toString());
+        String currentVersion = "";
+        try {
+            PackageInfo pInfo = requireContext().getPackageManager().getPackageInfo(requireContext().getPackageName(), 0);
+            currentVersion = pInfo.versionName;
+        } catch (PackageManager.NameNotFoundException ignored) {}
+
+        SharedPreferences mainPrefs = requireContext().getSharedPreferences("main_preference", Context.MODE_PRIVATE);
+        CheckBox checkBox = binding.automaticUpdateCheckBox;
+        checkBox.setChecked(mainPrefs.getBoolean("show_update_alert", true));
+        checkBox.setOnCheckedChangeListener((buttonView, isChecked) -> mainPrefs.edit().putBoolean("show_update_alert", isChecked).apply());
+
+        binding.textView3.setText(new StringBuilder().append("Version: "+ currentVersion).toString());
         return binding.getRoot();
     }
 
